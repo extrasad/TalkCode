4e5fa3df1ea39d0fa045c3ec9be671a7fcb3e288
Carlos Azuaje <carlosjazzc1@gmail.com>
2016-09-25 22:03:18 -0430

    new change 25/09/16

--- models.py
+++ models.py
@@ -8,13 +8,14 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120))
     password = db.Column(db.String(66))
     create_date = db.Column(db.DateTime, default=datetime.datetime.now)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, email):
         self.username = username
         self.password = self.__create_password(password)
+        self.email = email
 
     def __create_password(self, password):
         return generate_password_hash(password)
--- views.py
+++ views.py
@@ -17,58 +17,101 @@ mysql = MySQL(app)
 
 #Crear index que sustituya a users como pagina de inicio
 
+
+@app.errorhandler(404)
+def page_not_found():
+    return 'Page not found'
+
 #Probar como resulta esta nueva funcion
 @app.before_request
 def before_request():
-    if 'username' not in session:
-        print 'the user need login'
-    print 'before_request'
+    if 'username' not in session and request.endpoint in ['user']:
+        redirect(url_for('login'))
+        print '\033[94m'+'[the user need login]'.center(250)
+    elif 'username' in session and request.endpoint in ['register', 'index']:
+        redirect(url_for('user'))
+    else:
+        print '\033[94m'+'[   before_request  ]'.center(250)
 
 @app.route('/')
 def index():
-    return redirect(url_for('login'))
+    return redirect(url_for('register'))
 
 
 @app.route('/user', methods=['GET', 'POST'])
 def user():
     if 'username' in session:
         username = session['username']
-        print username
+        print '\033[4m'+'{}'.format(username).center(250)
         GeneralForm = form.GeneralForm(request.form)
         if request.method == 'POST' and not GeneralForm.validate():
             session['skill'] = GeneralForm.skill.data
             skill.append(session['skill'])
-            print skill
+            print '\033[4m'+'{}'.format(skill).center(250)
             return render_template('user.html', form=GeneralForm, name_user=session['username'], skill=skill)
         else:
             return render_template('user.html', form=GeneralForm, name_user=session['username'])
     else:
-        return redirect(url_for('login'))
+        return redirect(url_for('register')) #Login podria ser
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     GeneralForm = form.GeneralForm(request.form)
     if request.method == 'POST' and not GeneralForm.validate():
         username = GeneralForm.username.data
         password = GeneralForm.password.data
+        email = GeneralForm.email.data
 
         session['username'] = username
         session['password'] = password
+        session['email'] = email
 
-        user_new = User(username=GeneralForm.username.data,
-                        password=GeneralForm.password.data,
+        user_new = User(username=session['username'],
+                        password=session['password'],
+                        email=session['email']
                         )
         db.session.add(user_new)
         db.session.commit()
+        print '\033[92m'+'New user register!'.center(250)
         return redirect(url_for('user', name=username))
     else:
         return render_template('index.html', form=GeneralForm)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    Loginform = form.GeneralForm(request.form)
+    if request.method == 'POST' and not Loginform.validate():
+        username = Loginform.username.data
+        password = Loginform.password.data
+
+        user = User.query.filter_by(username=username).first()
+        if user is not None and user.verify_password(password):
+            succes_message = 'Bienvenido {}'.format(username)
+            flash(succes_message)
+            session['username'] = username
+            print '\033[92m'+'Bienvenido de nuevo {}'.format(username).center(250)
+            return redirect(url_for('user', name=username))
+
+
+
+        else:
+            error_message = 'Username or password invalid'
+            flash(error_message)
+            print '\033[93m'+'Error','\033[91m'+'\nusername:{}\npassword:{}'.format(username, password).center(250)
+            return redirect(url_for('login',))
+
+
+    return render_template('login.html', form=Loginform)
+
+
+
+
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
-    print session['username']
+    print session['username'].center(250)
     session.pop('username', None)
     return redirect(url_for('login'))
 
@@ -110,6 +153,7 @@ def users(name='default'):
 """
 
 if __name__ == '__main__':
+    """run csrf sucurity"""
     csrf.init_app(app)
     """"run the SQLAlchemy.create_all() method to create the tables and database:"""
     db.init_app(app)
