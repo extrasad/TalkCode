8f689c5dd7227e8d38ce5cbb216211dd42df1131
charlyjazz <carlosjazzc1@gmail.com>
2017-12-27 21:32:33 -0400

    Add test for user model and configure travis

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,17 @@
-from app.extensions import db
+from ..database import db, Model , Column, relationship
 
 import datetime
 
 
-class User(db.Model):
+class User(Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(66))
-    username = db.Column(db.String(80), unique=True)
-    create_date = db.Column(db.DateTime, default=datetime.datetime.now)
+    id = Column(db.Integer, primary_key=True)
+    email = Column(db.String(120), unique=True)
+    password = Column(db.String(66))
+    username = Column(db.String(80), unique=True)
+    created_at = Column(db.DateTime, default=datetime.datetime.now)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+from ..app.extensions import db as _db
+from ..app import create_app
+from ..app.config import TestConfig
+
+import pytest
+
+@pytest.fixture
+def app():
+    """An application for the tests."""
+    _app = create_app(TestConfig)
+    ctx = _app.test_request_context()
+    ctx.push()
+
+    yield _app
+
+    ctx.pop()
+
+
+@pytest.fixture
+def db(app):
+    """A database for the tests."""
+    _db.app = app
+    with app.app_context():
+        _db.create_all()
+
+    yield _db
+
+    # Explicitly close DB connection
+    _db.session.close()
+    _db.drop_all()
